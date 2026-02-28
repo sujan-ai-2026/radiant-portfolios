b5faa6646393d4be4d1f9d66e20b41d5fcf7e594
pirai-sujan <sujanp@piraiinfo.com>
2026-02-28 16:57:47 +0530

    jhd

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/radiant-portfolios">
+        <BrowserRouter basename="/sujan-portfolio">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/projects/:id" element={<ProjectDetail />} />
